e018560e15365bb40779c2f80ab17a2391984ebc
AbdulSattarMajid <malikabdulsattarmajid@gmail.com>
2025-07-31 14:18:58 +0500

    update backedn hosting

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import userRoutes from './Routes/userRoutes.js';
 import connectDB from './config/db.js'; 
 import productRoutes from './Routes/productRoutes.js';
 import categoryRoutes from './Routes/categoryRoutes.js'
+import orderRoutes from './Routes/orderRoutes.js'
+import sellerRoutes from './Routes/sellerRoutes.js'
+import wishlistRoutes from './Routes/wishlistRoutes.js'
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -14,18 +18,15 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 // DB Connection
 connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-
-
 app.use(cors({
-  origin: 'https://frontend4ecommerce.netlify.app',
-  credentials: true,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
 }));
 
 // Routes
@@ -33,6 +34,10 @@ app.use('/api/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/orders', orderRoutes);
+app.use('/api/seller', sellerRoutes)
+app.use("/api/wishlist", wishlistRoutes);
+
 
 // Server
 const PORT = process.env.PORT || 5000;
